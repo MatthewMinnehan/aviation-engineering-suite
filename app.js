8ad7c5ea85baa2f6b9eec8e08481abde65ec3525
Matthew Minnehan <104856526+MatthewMinnehan@users.noreply.github.com>
2026-06-15 15:56:23 -0600

    Add density altitude calculator

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
-document.getElementById("calculateBtn").addEventListener("click", () => {
+document
+.getElementById("calculateBtn")
+.addEventListener("click", () => {
 
-    const elevation =
-        Number(document.getElementById("elevation").value);
+    const aircraft =
+        document.getElementById("aircraft").value;
 
     const temperature =
-        Number(document.getElementById("temperature").value);
+        Number(
+            document.getElementById("temperature").value
+        );
 
     const pressureAltitude =
-        Number(document.getElementById("pressureAltitude").value);
+        Number(
+            document.getElementById("pressureAltitude").value
+        );
 
     const densityAltitude =
         pressureAltitude +
         (120 * (temperature - 15));
 
-    document.getElementById("result").innerHTML =
-        `Estimated Density Altitude: ${densityAltitude.toFixed(0)} ft`;
+    let condition = "";
+    let color = "";
 
+    if (densityAltitude < 3000) {
+        condition = "SAFE";
+        color = "#22c55e";
+    }
+    else if (densityAltitude < 6000) {
+        condition = "CAUTION";
+        color = "#f59e0b";
+    }
+    else {
+        condition = "HIGH RISK";
+        color = "#ef4444";
+    }
+
+    const result =
+        document.getElementById("result");
+
+    result.innerHTML =
+        `${densityAltitude.toFixed(0)} ft`;
+
+    result.style.color = color;
+
+    document
+        .getElementById("conditionBox")
+        .innerHTML =
+        `
+        Aircraft: ${aircraftDatabase[aircraft].name}<br>
+        Condition: ${condition}<br>
+        Cruise Speed: ${aircraftDatabase[aircraft].cruiseSpeed} knots
+        `;
 });
