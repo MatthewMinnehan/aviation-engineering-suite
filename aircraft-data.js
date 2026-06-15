d30d56f9bfb243a88b66f582aa53e82512b05d4d
Matthew Minnehan <104856526+MatthewMinnehan@users.noreply.github.com>
2026-06-15 15:55:05 -0600

    Create aircraft data

---
+++ aircraft-data.js
@@ -0,0 +1,24 @@
+const aircraftDatabase = {
+
+    c172: {
+        name: "Cessna 172",
+        cruiseSpeed: 122,
+        maxWeight: 2550,
+        fuelCapacity: 56
+    },
+
+    da40: {
+        name: "Diamond DA40",
+        cruiseSpeed: 147,
+        maxWeight: 2646,
+        fuelCapacity: 50
+    },
+
+    sr22: {
+        name: "Cirrus SR22",
+        cruiseSpeed: 183,
+        maxWeight: 3600,
+        fuelCapacity: 92
+    }
+
+};
